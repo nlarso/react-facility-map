8742efb4611b65de13973e77d7deb2611fb32377
nlarso <nlarso@gmail.com>
2018-03-08 12:01:11 -0700

    Install React via react-rails and webpacker

---
+++ app/javascript/components/Sitemap.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Sitemap extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>
+          Hello World!
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Sitemap
